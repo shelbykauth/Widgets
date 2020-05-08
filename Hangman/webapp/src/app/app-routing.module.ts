d6d0aa8c20a1021525c78dff0d798c9671fbd0f4
ShelbyKauth <shelbykauth@gmail.com>
2020-05-07 22:14:59 -0400

    Move Placeholder to AngularHelp page

--- Hangman/webapp/src/app/app-routing.module.ts
+++ Hangman/webapp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AngularHelpComponent } from './angular-help/angular-help.component';
 import { GameScreenComponent } from './game-screen/game-screen.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 const routes: Routes = [
+  { path: 'AngularHelp', component: AngularHelpComponent },
   { path: 'AboutComponent', component: AboutComponent },
   { path: 'GameScreenComponent', component: GameScreenComponent },
   { path: 'HomeScreenComponent', component: HomeScreenComponent },
--- Hangman/webapp/src/app/app.module.ts
+++ Hangman/webapp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { GameScreenComponent } from './game-screen/game-screen.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { AngularHelpComponent } from './angular-help/angular-help.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeScreenComponent } from './home-screen/home-screen.component';
     AboutComponent,
     GameScreenComponent,
     HomeScreenComponent,
+    AngularHelpComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
