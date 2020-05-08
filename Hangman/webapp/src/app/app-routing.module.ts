8cbaeaee75ad5c13d69219d26a2911753d03767f
ShelbyKauth <shelbykauth@gmail.com>
2020-05-07 22:04:47 -0400

    Add Components to route

--- Hangman/webapp/src/app/app-routing.module.ts
+++ Hangman/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'AboutComponent', component: AboutComponent },
+  { path: 'GameScreenComponent', component: GameScreenComponent },
+  { path: 'HomeScreenComponent', component: HomeScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
