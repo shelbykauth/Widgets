fc702385cd7d823cda9f269c1be799a4cbd8d90e
ShelbyKauth <shelbykauth@gmail.com>
2020-05-10 13:28:17 -0400

    Rearrange Module/Component imports

--- Hangman/webapp/src/app/app.module.ts
+++ Hangman/webapp/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { GameScreenComponent } from './game-screen/game-screen.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { AngularHelpComponent } from './angular-help/angular-help.component';
 import { GallowsImageComponent } from './gallows-image/gallows-image.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
