562f6cd8c182d7c443f996fc0a63d9229f6e826a
ShelbyKauth <shelbykauth@gmail.com>
2020-05-10 17:06:36 -0400

    Cleanup

--- webapp/src/app/games/hangman/hangman.component.ts
+++ webapp/src/app/games/hangman/hangman.component.ts
@@ -195,76 +195,6 @@ export class HangmanComponent implements OnInit {
     hiding.saved_man = !this.hasWon;
     hiding.hanged_man = this.hasWon;
     return hiding;
-    const total = this.totalGuesses;
-    const wrong = this.wrongGuesses;
-    const left = total - wrong;
-    const fractionWrong = wrong / total;
-    const partsPerGuesses = {
-      6: {
-        head: wrong < 1,
-        spine: wrong < 2,
-        left_arm: wrong < 3,
-        right_arm: wrong < 4,
-        left_leg: wrong < 5,
-        right_leg: wrong < 6,
-        frown: wrong < 6,
-      },
-      5: {
-        head: wrong < 1,
-        spine: wrong < 2,
-        left_arm: wrong < 3,
-        right_arm: wrong < 4,
-        left_leg: wrong < 5,
-        right_leg: wrong < 5,
-        frown: wrong < 5,
-      },
-      4: {
-        head: wrong < 1,
-        spine: wrong < 2,
-        arms: wrong < 3,
-        left_arm: wrong < 3,
-        right_arm: wrong < 3,
-        legs: wrong < 4,
-        left_leg: wrong < 4,
-        right_leg: wrong < 4,
-        frown: wrong < 4,
-      },
-      3: {
-        head: wrong < 1,
-        spine: wrong < 1,
-        arms: wrong < 2,
-        left_arm: wrong < 2,
-        right_arm: wrong < 2,
-        legs: wrong < 3,
-        left_leg: wrong < 3,
-        right_leg: wrong < 3,
-        frown: wrong < 3,
-      },
-      2: {
-        head: wrong < 1,
-        spine: wrong < 1,
-        arms: wrong < 2,
-        left_arm: wrong < 2,
-        right_arm: wrong < 2,
-        legs: wrong < 2,
-        left_leg: wrong < 2,
-        right_leg: wrong < 2,
-        frown: wrong < 2,
-      },
-      1: {
-        head: wrong < 1,
-        body: wrong < 1,
-        spine: wrong < 1,
-        arms: wrong < 1,
-        left_arm: wrong < 1,
-        right_arm: wrong < 1,
-        legs: wrong < 1,
-        left_leg: wrong < 1,
-        right_leg: wrong < 1,
-        frown: wrong < 1,
-      },
-    };
-    return partsPerGuesses[this.totalGuesses];
   }
 
   set hiddenPhrase(val) {
@@ -276,7 +206,6 @@ export class HangmanComponent implements OnInit {
     this.hiddenCharacters.forEach((char) => {
       let letter = this.lettersByName[char];
       if (letter) {
-        console.log(letter);
         letter.inWord = true;
       }
     });
@@ -301,7 +230,6 @@ export class HangmanComponent implements OnInit {
       }
     });
     return displayCharacters.join('\u00A0');
-    return "H A _ _ Y   M O _ H _ R ' _   D A Y";
   }
 
   get hasWon() {
